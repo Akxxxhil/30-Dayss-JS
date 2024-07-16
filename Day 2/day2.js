b1cdb77e88bb2736ed19fe34e17a14cb9e8ae56c
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-07-16 21:29:08 +0530

    Day 2

---
+++ Day 2/day2.js
@@ -0,0 +1,76 @@
+// Activity 1 : Arithmetic Operations
+
+
+let a=20;
+let b=30;
+
+// Task 1
+console.log(a+b);
+// Task 2
+console.log(a-b);
+// Task 3
+console.log(a*b);
+// Task 4
+console.log(a/b);
+
+// Task 5 
+console.log(6%3);
+
+
+// Activity 2 : Assignment Operators
+
+//Task 6 
+let c=30;
+c=c+20;
+console.log(c);
+
+// Task 7
+let d=30;
+d=d-20;
+console.log(d);
+
+// Activity 3 : Comaparison Operators
+
+//Task 8
+console.log(8>9);
+//Task 9
+console.log(8>=9);
+//Task 10
+console.log(8==9);
+
+
+// Activity 4 : Logical Operators
+
+// Task 11
+if( 10 > 9 && 10<12){
+    console.log("yes");
+}
+else{
+    console.log("No");
+}
+// Task 12
+
+if( 10 > 9 || 10<12){
+    console.log("yes");
+}
+else{
+    console.log("No");
+}
+// Task 13
+let akhil=2;
+let akhil2=4;
+if(akhil !=akhil2){
+    console.log("Not operator,yes");
+}
+else{
+    console.log("Not operator,,no");
+}
+
+// Activity 5 : Ternary Opeartor
+
+let ans=2;
+
+ans >= 2 ? console.log("greater than or equal 2"):console.log("Not greater than 2");
+
+
+
